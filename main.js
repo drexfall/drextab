0a2174381465029c259c9ca54b187f2beed410f7
drexfall <67905454+drexfall@users.noreply.github.com>
2022-08-08 04:16:30 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let searchbar, form, selector;
 function bodyLoad() {
   searchSelector = document.getElementById("defSearch");
+  searchButton = document.getElementById("btnSearch");
   searchbar = document.getElementById("mainSearch");
   form = document.getElementById("searchForm");
 
@@ -9,10 +10,9 @@ function bodyLoad() {
   let engines = settings.search.engines;
   let index = 0;
   for (let engine in engines) {
-    searchSelector.innerHTML+=`\n<option value="${engine}">${engine}</option>`
-
+    searchSelector.innerHTML += `\n<option value="${engine}">${engine}</option>`;
   }
-  searchSelector.value = settings.search.default
+  searchSelector.value = settings.search.default;
   if (settings.search.autofocus) {
     searchbar.focus();
   } else {
@@ -26,4 +26,20 @@ function setSearch() {
   searchbar.name = engine.query;
   form.target = settings.search.newtab ? "_blank" : "_self";
   form.action = engine.url;
+}
+function onType() {
+  let urlregex = /\w+(\.\w+)+/;
+  let httpregex = /(http(s){0,1}:\/\/)/;
+  if (searchbar.value.match(urlregex)) {
+    if (searchbar.value.match(httpregex)) {
+      console.log(searchbar.value);
+      form.action = searchbar.value;
+    } else {
+      form.action = "https://" + searchbar.value;
+    }
+    searchButton.value = "Launch Website!"
+  } else {
+    setSearch();
+    searchButton.value = "Search"
+  }
 }
