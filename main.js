1f3a9972696c849af33d59e05952a40e5823db92
drexfall <67905454+drexfall@users.noreply.github.com>
2024-11-19 00:55:06 +0530

    Fixed search logic and url detection

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
-let searchbar, form, selector;
-function bodyLoad() {
+let searchbar, form, selector, searchSelector, searchButton;
+
+function setSearch() {
+  const engine = settings.search.engines[searchSelector.value];
+
+  searchbar.name = engine.query;
+  form.target = settings.search.newtab ? "_blank" : "_self";
+  form.action = engine.url;
+}
+function onType() {
+  let urlregex = /\w+(\.\w+)+/, httpregex = /(http(s){0,1}:\/\/)/;
+  if (searchbar.value.match(urlregex)) {
+    if (searchbar.value.match(httpregex)) {
+      form.action = searchbar.value;
+      form.method = "get"
+    } else {
+      form.action = "https://" + searchbar.value;
+      form.method = "post"
+    }
+    searchButton.textContent = "Launch"
+  } else {
+    setSearch();
+    searchButton.textContent = "Search"
+  }
+}
+
+window.addEventListener("load",()=>{
+  initialSet(true)
   searchSelector = document.getElementById("defSearch");
   searchButton = document.getElementById("btnSearch");
   searchbar = document.getElementById("mainSearch");
@@ -8,7 +34,6 @@ function bodyLoad() {
   searchbar.value = "";
 
   let engines = settings.search.engines;
-  let index = 0;
   for (let engine in engines) {
     searchSelector.innerHTML += `\n<option value="${engine}">${engine}</option>`;
   }
@@ -19,27 +44,4 @@ function bodyLoad() {
     searchbar.blur();
   }
   setSearch();
-}
-function setSearch() {
-  const engine = settings.search.engines[searchSelector.value];
-
-  searchbar.name = engine.query;
-  form.target = settings.search.newtab ? "_blank" : "_self";
-  form.action = engine.url;
-}
-function onType() {
-  let urlregex = /\w+(\.\w+)+/;
-  let httpregex = /(http(s){0,1}:\/\/)/;
-  if (searchbar.value.match(urlregex)) {
-    if (searchbar.value.match(httpregex)) {
-      console.log(searchbar.value);
-      form.action = searchbar.value;
-    } else {
-      form.action = "https://" + searchbar.value;
-    }
-    searchButton.value = "Launch"
-  } else {
-    setSearch();
-    searchButton.value = "Search"
-  }
-}
+})
